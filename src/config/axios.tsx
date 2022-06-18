addc2239bdbce93d51270dff2e8448c6398d19d4
Krystian Gaj <krystian0120@outlook.com>
2022-06-19 00:50:10 +0200

    Remove security hotspots

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -4,7 +4,7 @@ import {environment} from "./environment";
 const axios = axiosFactory.create({
     baseURL: environment.serverURL,
     headers: {
-        "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Origin": "*",
         // "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
     }
 })
